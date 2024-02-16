60e7e72f25dc7a1f750601896a697bca2b97fa29
Ryan Sheridan <ryan.sheridan@cuanschutz.edu>
2024-02-15 22:53:36 -0700

    update structure

--- build_pubs.R
+++ build_pubs.R
@@ -1,42 +1,57 @@
-library(markdown)
 library(yaml)
 library(tidyverse)
 library(glue)
 library(here)
 library(cli)
+library(rvest)
 
 # Directories
-pub_info  <- read_yaml("pubs.yml")
-athr_info <- read_yaml("authors.yml")
+pub_yml   <- read_yaml("pubs.yml")
+athr_yml  <- read_yaml("authors.yml")
 pub_dir   <- here("pubs")
 atlas_dir <- here("atlases")
 img_dir   <- here("images")
 template  <- "jolla"
+
+# Just need this to order categories
+projs <- names(read_yaml("projects.yml"))
 
 if (!dir.exists(pub_dir))   dir.create(pub_dir)
 if (!dir.exists(atlas_dir)) dir.create(atlas_dir)
 
 # Info for each link
 link_info <- list(
-  publication = list(text = "Publication", icon = "file-earmark"),
-  atlas       = list(text = "Atlas", icon = "compass"),
-  github      = list(text = "GitHub", icon = "github"),
-  geo         = list(text = "NCBI GEO")
+  pubmed = list(text = "Publication", icon = "file-earmark"),
+  atlas  = list(text = "Atlas", icon = "compass"),
+  github = list(text = "GitHub", icon = "github"),
+  geo    = list(text = "NCBI GEO")
 )
 
 # Template to write index.qmd
 index_qmd <- "---\ntitle: {ttl}\n"
 
 # Create index.qmd for each pub entry
-pub_info %>%
-  iwalk(~ {
-    info  <- .x
-    links <- names(info)
-    links <- links[links %in% names(link_info)]
-    links <- link_info[links]
-    pub   <- here(pub_dir, .y)
-    ttl   <- info$title
-    cats  <- str_c(info$categories, collapse = ", ")
+pub_yml <- pub_yml %>%
+  imap(~ {
+    pub_info <- .x
+    
+    if (is.null(pub_info$title)) {
+      pub_info <- .scrape_pubmed(.x$pubmed)
+      pub_info <- append(.x, pub_info)
+    }
+    
+    ttl <- pub_info$title
+    pub <- here(pub_dir, pub_info$key)
+    
+    # Set link icons and text
+    links <- append(pub_info["pubmed"], pub_info$links)
+    
+    links <- links %>%
+      imap(~ {
+        lnk <- list(link = .x)
+        
+        append(lnk, link_info[[.y]])
+      })
     
     # Create pub directory
     if (!dir.exists(pub)) dir.create(pub)
@@ -44,7 +59,7 @@ pub_info %>%
     # Pull image
     # image name must match pub directory name
     # conditionally add image field
-    img <- dir(img_dir, str_c("^", .y, "\\."), full.names = TRUE)
+    img <- dir(img_dir, str_c("^", pub_info$key, "\\."), full.names = TRUE)
     
     if (length(img) > 1) {
       cli_warn("Multiple images found, using {img[[1]]}")
@@ -63,49 +78,82 @@ pub_info %>%
     
     index_qmd <- str_c(
       index_qmd,
-      "categories: [{cats}]\n",
+      "categories: [{cats_str}]\n",
       "about:\n",
       "  id: about\n",
       "  template: {template}\n",
       "  links:\n"
     )
     
+    cats <- athr_yml[pub_info$authors] %>%
+      map(pluck, "project") %>%
+      discard(is.null) %>%
+      reduce(c)
+    
+    cats <- c(
+      intersect(projs, cats),
+      sort(setdiff(cats, projs))
+    )
+    
+    cats_str <- cats %>%
+      str_c(collapse = ", ")
+    
     # Parse author link info
-    athrs <- info$authors %>%
+    athrs <- pub_info$authors %>%
       map_chr(~ {
-        lnk <- athr_info[[.x]]$publications
+        lnk <- athr_yml %>%
+          pluck(.x, "publications")
         
         if (!is.null(lnk)) .x <- str_c("[", .x, "](", lnk, ")")
         .x
       }) %>%
       str_c(collapse = ", ")
-
-    # Parse pub link info
+    
+    # Copy files for atlases
+    # symbolic links do not seem to work
+    atlas_lnk <- here(atlas_dir, pub_info$key)
+    
+    if (!is.null(links$atlas) && !file.exists(atlas_lnk)) {
+      file.copy(pub, atlas_dir, recursive = TRUE)
+    }
+    
+    # Parse provided links for publication
     links <- links %>%
       imap(~ {
         vals <- list()
         if (!is.null(.x$text))    vals$text <- str_c("  - text: ", .x$text)
         if (!is.null(.x$icon))    vals$icon <- str_c("    icon: ", .x$icon)
-        if (!is.null(info[[.y]])) vals$href <- str_c("    href: ", info[[.y]])
+        if (!is.null(.x$link))    vals$href <- str_c("    href: ", .x$link)
         
         str_c(str_c(vals, collapse = "\n"), "\n")
       }) %>%
-      reduce(str_c)
+      str_c(collapse = "")
+    
+    # Update authors.yml with new authors from publication
+    missing_athrs <- pub_info$authors[!pub_info$authors %in% names(athr_yml)]
+    
+    athr_yml[missing_athrs] <- NA
+    
+    athr_yml %>%
+      write_yaml(here("authors.yml"))
     
     # Write final index.qmd
     index_qmd <- glue(
       index_qmd, links, "---\n\n",
       athrs, "\n\n",
-      info$abstract
+      pub_info$abstract
     )
     
-    # Copy files for atlases
-    # symbolic links do not seem to work
-    atlas_lnk <- here(atlas_dir, .y)
-    
-    if (!is.null(info$atlas) && !file.exists(atlas_lnk)) {
-      file.copy(pub, atlas_dir, recursive = TRUE)
-    }
-    
     write_lines(index_qmd, here(pub, "index.qmd"))
+    
+    # Update pubs.yml with categories derived from the authors
+    # return updated pub_yml
+    pub_info$project <- cats
+    
+    pub_info
   })
+
+# Write updated pubs.yml
+# this will have info scraped from pubmed
+pub_yml %>%
+  write_yaml(here("pubs.yml"))
--- funs.R
+++ funs.R
@@ -1,41 +1,120 @@
-.create_project_links <- function(athr_info, pub_info, lnk_cls) {
+.create_project_links <- function(proj_yml, athr_yml, pub_yml, lnk_cls) {
   
   # Identify publications and atlases listed for each project
   # only want to include links if there is something to link to
-  pubs <- pub_info %>%
-    map_chr(pluck, "categories") %>%
-    unique()
-  
-  atlases <- pub_info %>%
-    map(pluck, "atlas") %>%
-    discard(is.null) %>%
-    names()
-  
-  atlases <- pub_info[atlases] %>%
-    map_chr(pluck, "categories") %>%
-    unique()
-  
-  # Create links
-  res <- athr_info %>%
-    map(pluck, "project") %>%
-    discard(is.null) %>%
+  res <- proj_yml %>%
     imap(~ {
-      nm     <- str_c(.y, ", PhD")
-      nm_lnk <- str_to_lower(nm)
-      nm_lnk <- str_replace_all(nm_lnk, "( |, )", "-")
+      proj <- .y
       
-      lnk <- str_c("[", nm, "](investigators.qmd#", nm_lnk, ")", lnk_cls)
+      # Links to investigators page
+      athrs <- athr_yml %>%
+        keep(~ is.list(.x) && proj %in% .x$project) %>%
+        names()
       
-      if (.x %in% pubs) {
-        lnk <- str_c(lnk, " [Publications](pubs.qmd#category=", .x, ")", lnk_cls)
+      lnks <- athrs %>%
+        map(~ {
+          nm <- str_c(.x, ", PhD")
+          
+          nm_lnk <- nm %>%
+            str_to_lower() %>%
+            str_replace_all("( |, )", "-")
+          
+          str_c("[", nm, "](investigators.qmd#", nm_lnk, ")", lnk_cls)
+        }) %>%
+        str_c(collapse = " ")
+      
+      # Link to publications
+      pubs <- pub_yml %>%
+        map_lgl(~ proj %in% .x$project)
+        
+      if (any(pubs)) {
+        lnks <- str_c(
+          lnks, " [Publications](pubs.qmd#category=", proj, ")",
+          lnk_cls
+        )
       }
       
-      if (.x %in% atlases) {
-        lnk <- str_c(lnk, " [Atlases](atlases.qmd#category=", .x, ")", lnk_cls)
+      # Link to atlases
+      atlases <- pub_yml[pubs] %>%
+        map_lgl(~ !is.null(.x$links$atlas))
+      
+      if (any(atlases)) {
+        lnks <- str_c(
+          lnks, " [Atlases](atlases.qmd#category=", proj, ")",
+          lnk_cls
+        )
       }
       
-      lnk
+      lnks
     })
+  
+  res
+}
+
+.scrape_pubmed <- function(url) {
+  html <- read_html(url)
+  
+  # Scape author list
+  # modify to only include middle initials
+  athrs <- html %>%
+    html_elements("a.full-name") %>%
+    map_chr(html_text2) %>%
+    unique()
+  
+  athrs <- athrs %>%
+    str_split(" ") %>%
+    map_chr(.format_name)
+  
+  # Scrap title and abstract
+  ttl <- html %>%
+    html_elements("h1.heading-title") %>%
+    map_chr(html_text2) %>%
+    pluck(1)
+  
+  abst <- html %>%
+    html_elements("p") %>%
+    map_chr(html_text2) %>%
+    pluck(4)
+  
+  # Set publication key based on first author and key
+  # this is used to create page directories
+  yr <- html %>%
+    html_elements("span.cit") %>%
+    pluck(1) %>%
+    html_text2() %>%
+    str_extract("^[0-9]{4}(?= )")
+  
+  if (is.na(yr)) cli_abort("Error extracting year from pubmed link")
+  
+  athr1 <- athrs[1] %>%
+    str_extract("[^ ]+$") %>%
+    str_to_lower()
+  
+  key <- str_c(yr, "-", athr1)
+  
+  # Return publication info
+  res <- list(
+    key      = key,
+    title    = ttl,
+    year     = yr,
+    authors  = athrs,
+    abstract = abst
+  )
+  
+  res
+}
+
+.format_name <- function(nm) {
+  frst <- first(nm)
+  lst  <- last(nm)
+  
+  mid <- nm[!nm %in% c(frst, lst)]
+  
+  mid <- mid %>%
+    str_extract("^[a-zA-Z]")
+  
+  res <- c(frst, lst) %>%
+    str_c(collapse = " ")
   
   res
 }
