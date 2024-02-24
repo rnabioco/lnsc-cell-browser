dfff5e84be724db396ca5e94186e5ddf3fc3030d
Ryan Sheridan <ryan.sheridan@cuanschutz.edu>
2024-02-24 14:01:51 -0700

    adjust project tags, timeline labels

--- pre-build.R
+++ pre-build.R
@@ -113,6 +113,7 @@ tl_dat <- pub_yml_new %>%
     tibble(
       key     = .x$key,
       date    = as.Date(str_c(.x$year, "-01-01")),
+      proj    = str_c(str_to_title(.x$project), collapse = ", "),
       authors = str_c(athrs, collapse = ", "),
       pmid    = str_extract(.x$pubmed, "[0-9]+(/|)$")
     )
@@ -123,7 +124,7 @@ tl_dat <- tl_dat %>%
   mutate(
     pmid = str_remove(pmid, "/$"),
     pmid = str_c("PMID", pmid),
-    lab  = str_c(authors, "\n", pmid),
+    lab  = str_c(proj, "\n", pmid),
     year = as.character(year(date)),
     url  = file.path(pub_dir, key)
   )
